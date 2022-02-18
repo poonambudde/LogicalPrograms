a387a71832f0d10fdf8e44bc42515e455cfcc678
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-18 18:06:28 +0530

    [Poonam] ADDED: method to find a Perfect Number

---
+++ Perfect Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Perfect_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, n;
+            Console.Write("Enter the Number : ");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\nEntered number is a perfect number");
+             
+            }
+            else
+            {
+                Console.WriteLine("\nEntered number is not a perfect number");
+             
+            }
+        }
+    }
+}
