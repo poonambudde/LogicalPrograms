f592bd6a3b8d38e12db7171e3f32d0143ce981a1
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-21 00:09:29 +0530

    [Poonam] ADDED: Calculate Monthly Payment

---
+++ Monthly Payment/Monthly Payment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MonthlyPayment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculate monthly payment");
+            Console.WriteLine("Enter the value of principal amount: ");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the year: ");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter percent interest");
+            double R = Convert.ToDouble(Console.ReadLine());
+            GetMonthlyPayment(P, Y, R);
+        }
+
+        public static void GetMonthlyPayment(double P, double Y, double R)
+        {
+           
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+            double payment = P * r / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine(payment +  " is monthly payment");
+        }
+    }
+}
+
