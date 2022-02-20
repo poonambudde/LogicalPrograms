ec6568e6942dad86ab177a5e4eb0827acdf93b6e
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-21 00:12:56 +0530

    [Poonam] ADDED: Compute sqrt of nonnegative number c

---
+++ Square Root/Square Root/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate Square Root using Newtons Method");
+            Console.WriteLine("Enter Non negative Number :-");
+            double c = Convert.ToDouble(Console.ReadLine());
+            GetRoot(c);
+        }
+        public static void GetRoot(double c)
+        {
+            double t = c;
+            while ((Math.Abs(t - c / t)) > (1e-15 * t))
+            {
+                t = (t + (c / t)) / 2;
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
